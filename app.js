31da5a7cb9602f6e3429f4a9504d139c0265d016
blandero <landerosb08@gmail.com>
2023-10-26 16:08:16 -0600

    intial commit

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 // ******* STATE *******
-const state = {};
+const state = {
+  groceries: [
+    {name: "apple",quantity: 2, price: 1.75 }, 
+    {name: "banana", quantity: 4, price: 0.25},
+  ],
+};
 
 const items = [
   { name: "apple", price: 1.75 },
@@ -17,9 +22,48 @@ const items = [
 ];
 
 // ******* REFERENCES *******
+const form = document.querySelector(".grocery-form")
+const tableBody = document.getElementById("table-body");
+const clearButton = document.querySelector(".clear-btn");
+console.log("body: ", tableBody);
+
 
 // ******* EVENT LISTENERS *******
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const inputValue = form.nextElem;
+  const qty = Math.floor(Math.random() * 10);
+  const price = Math.floor(Math.random() * 5);
+
+  state.groceries.push({name: inputValue, quantiy: qty, price});
+  
+  console.log("groceries", state.groceries);
+});
+render();
+
 
 // ******* RENDER *******
+function render() {
+  // we want to display all our groceries in the table
+  const rowItems = state.groceries.map((item) => {
+    console.log(item);
+    const newRow = document.createElement("tr")
+    
+    const name = document.createElement("td")
+    name.textContent = item.name;
+
+    const quantity = document.createElement("td")
+    qty.textContent = item.quantity;
+
+    const price = document.createElement("td")
+    price.textContent = item.price;
+
+    newRow.replaceChild(name, qty, price);
+
+    return newRow
+  });
+  console.log("row items: ", rowItems);
+  tableBody.replaceChild(...rowItems);
+}
 
 // ******* FUNCTIONS *******
